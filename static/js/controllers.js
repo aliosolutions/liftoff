42bf0c9736833da9e0e3761d2c623c62af42e225
Judd Keppel <kepzorz@gmail.com>
2013-02-14 14:35:31 -0800

    Show styling

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,32 @@
 
 
 function ArtistCtrl($scope){
-	$scope.artist = "Justin and The Goughs"
+	$scope.artist = {
+		name: 'Justin and The Goughs',
+		description: "Triple threat musician, comedian, entrepreneur Justin Gough. See his unforgettable one-man show.",
+		pic: "http://placehold.it/640x480"
+	};
+
+	var testShow = {
+		city: "Seattle",
+		tix: {
+			sold: 30,
+			goal: 50
+		},
+		percent: Math.floor(100 * 30 / 50),
+		daysLeft: 4
+	};
+	var otherShow = {
+		tix: {
+			sold: 20,
+			goal: 60
+		},
+		percent: Math.floor(100 * 20 / 60),
+		city: "San Francisco",
+		daysLeft: 4
+	};
+
+
+	
+	$scope.shows = [testShow, otherShow]
 }
