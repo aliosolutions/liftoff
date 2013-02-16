1f47e8e485e7f4a7da58cbdaced5c90802fdccd8
Judd Keppel <kepzorz@gmail.com>
2013-02-15 17:28:26 -0800

    Stripe token generation

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -25,8 +25,53 @@ function ArtistCtrl($scope){
 		city: "San Francisco",
 		daysLeft: 4
 	};
+	var oneMoreShow = {
+		tix: {
+			sold: 45,
+			goal: 300
+		},
+		percent: Math.floor(100 * 45 / 300),
+		city: "Portland",
+		daysLeft: 4,
+		price: 8000
+	};
+	$scope.selectedShow = {};
+	$scope.selectedArtist = {}
+	$scope.order = {
+		quantity: 1
+	}
+	$scope.openInfo = function(show, artist){
+		$scope.selectedShow = show;
+		$scope.selectedArtist = artist;
+		$('#infoModal').modal();
+	} 
+	$scope.openPayment = function(){
+		$('#infoModal').modal('hide');
+		console.log("Show price: " + $scope.selectedShow.price + ", artist name: " + $scope.selectedArtist.name);
+		var token = function(res){
+			//pack everything up neatly into $scope.order
+			$scope.order.show = $scope.selectedShow;
+			$scope.order.show.artist = $scope.selectedArtist;
+			$scope.order.token = res;
+			$scope.order.amount = $scope.order.show.price * $scope.order.quantity;
+			$scope.placeOrder();
+		}
+		var show = $scope.selectedShow;
+		var artist = $scope.selectedArtist;
+		var order = $scope.order;
+		StripeCheckout.open({
+			key:         'pk_test_czwzkTp2tactuLOEOqbMTRzG',
+	        amount:      show.price * order.quantity,
+	        name:        artist.name,
+	        description: "" + order.quantity + " ticket(s)($" + show.price / 100 + " each) to show in " + show.city,
+	        panelLabel:  'Buy ticket(s)',
+	        token:       token
+		});
+	}
+	$scope.placeOrder = function(){
+		console.log("Placing order");
+		console.log($scope.order)
+	}
 
-
-	
-	$scope.shows = [testShow, otherShow]
+	$scope.shows = [testShow, otherShow, oneMoreShow]
 }
