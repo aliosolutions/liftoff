af24be51c9231d2945ae8b4420989d8f95166095
Judd Keppel <kepzorz@gmail.com>
2013-03-20 16:40:55 -0700

    Added name-to-URL filter

--- static/js/filters.js
+++ static/js/filters.js
@@ -1,7 +1,12 @@
 
 app.filter('truncate', function(Data){
 	return function(text){
-		console.log("Truncation filter")
 		return text.substring(0, Data.truncationLength) + '...'
+	}
+});
+
+app.filter('URLify', function(Data){
+	return function(name){
+		return name.replace(/\s/g, '_')
 	}
 });
